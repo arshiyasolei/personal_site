f67dfef229a9a5f42fd53d1aa01f2e9e886516b0
arshiyasolei <arshiyasoleimani@gmail.com>
2020-12-15 16:14:56 -0800

    mostly done!

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    console.log("welcome to the site.")
+
+    const c = document.getElementById("mainCanvas");
+    var ctx = c.getContext("2d");
+
+    c.width  = window.innerWidth;
+    c.height = window.innerHeight;
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+
+    document.getElementById("download_resume").onclick = () => {
+        window.location = "/resume_arshia.pdf"
+    }
+}
